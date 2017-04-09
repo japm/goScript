49be6904e23f2a5c109aa0077a2df5dbd624564d
juan <juan.pascual@peixeurbano.com>
2017-04-09 12:26:20 -0300

    Call code refactor for clarity.
    Minor performance gains in call function benchmark

--- call.go
+++ call.go
@@ -65,80 +65,77 @@ func evalCallExpr(expr *ast.CallExpr, context Context) (interface{}, error) {
 
 	//Find the type called, this calls evalSelectorExpr/ evalIdent
 	val, callsite, err := evalFromCall(expr.Fun, context)
-	if err != nil {
-		return nilInterf, err
-	}
 
-	//-------------------Check Method------------------------
-	var vaArgsTp reflect.Type
-	var method reflect.Value
-	var methodCallable Callable
-	var isCallable bool
+	if err == nil {
 
-	if !callsite.isValid {
-		//Not a callSite, must be a Ident(args), so Ident must be a function
-		if val == nil {
-			return nilInterf, fmt.Errorf("Waiting reflect.Func/Callable found %v", "nil")
-		}
-		methodCallable, isCallable = val.(Callable)
-		if !isCallable {
-			method = reflect.ValueOf(val)
-			if method.Kind() != reflect.Func {
-				return nilInterf, fmt.Errorf("Waiting reflect.Func found %v", reflect.TypeOf(val))
+		var method reflect.Value
+		var callable Callable
+
+		//Get method to call, a callable or a reflect method
+		method, callable, err = getMethodToCall(val, callsite)
+
+		//Call
+		if err == nil {
+			if callable != nil {
+				val, err = evalCallExprCallable(expr, context, callable)
+			} else {
+				val, err = evalCallExprReflect(expr, context, method)
 			}
 		}
+	}
+
+	return val, err
+}
+
+func evalCallExprCallable(expr *ast.CallExpr, context Context, methodCallable Callable) (val interface{}, err error) {
+	var args []interface{}
+	if len(expr.Args) == 0 {
+		args = zeroArgInterf //Zero arg constant
 	} else {
-		callableContext, ok := callsite.callee.(CallableContext)
-		if ok {
-			methodCallable, err = callableContext.GetCallable(callsite.fnName)
-			isCallable = (err == nil)
-		} else {
-			isCallable = false
-		}
-		if !isCallable {
-			//A callSite so must be f.x(args)
-			caleeVal := reflect.ValueOf(callsite.callee)
-			method = caleeVal.MethodByName(callsite.fnName)
-			if !method.IsValid() {
-				return nilInterf, fmt.Errorf("Method %s not found", callsite.fnName)
+		args = make([]interface{}, len(expr.Args))
+
+		for key, value := range expr.Args {
+
+			v, e := eval(value, context)
+			if e != nil {
+				err = e
+				break
 			}
+			args[key] = v
 		}
 	}
-	if isCallable {
-		var args []interface{}
-		if len(expr.Args) == 0 {
-			args = zeroArgInterf //Zero arg constant
-		} else {
-			args = make([]interface{}, len(expr.Args))
-
-			for key, value := range expr.Args {
-
-				val, err := eval(value, context)
-				if err != nil {
-					return nilInterf, err
-				}
-				args[key] = val
-			}
-		}
-		return methodCallable.Call(args)
+	if err == nil {
+		val, err = methodCallable.Call(args)
 	}
+
+	return
+}
+
+func evalCallExprReflect(expr *ast.CallExpr, context Context, method reflect.Value) (val interface{}, err error) {
+	var vaArgsTp reflect.Type
+	var args []reflect.Value
+	var retVal []reflect.Value
 
 	mType := method.Type()
 	numArgs := mType.NumIn()
+
 	if !mType.IsVariadic() {
 		if len(expr.Args) != numArgs {
-			return nilInterf, fmt.Errorf("Method alguments count mismatch. Expected %d get %d", numArgs, len(expr.Args))
+			err = fmt.Errorf("Method alguments count mismatch. Expected %d get %d", numArgs, len(expr.Args))
 		}
 	} else {
 		numArgs = numArgs - 1
 		vaArgsTp = mType.In(numArgs).Elem() //Type declared
 		if len(expr.Args) < numArgs {
-			return nilInterf, fmt.Errorf("Method alguments count mismatch. Expected at least %d get %d", (numArgs - 1), len(expr.Args))
+			err = fmt.Errorf("Method alguments count mismatch. Expected at least %d get %d", (numArgs - 1), len(expr.Args))
 		}
 	}
 
+	if err != nil {
+		return
+	}
+
 	//-------------------Prepare call arguments ------------
-	var args []reflect.Value
 	if len(expr.Args) == 0 {
 		args = zeroArg //Zero arg constant
 	} else {
@@ -147,11 +144,12 @@ func evalCallExpr(expr *ast.CallExpr, context Context) (interface{}, error) {
 
 		for key, value := range expr.Args {
 
-			val, err := eval(value, context)
-			if err != nil {
-				return nilInterf, err
+			v, e := eval(value, context)
+			if e != nil {
+				err = e
+				break
 			}
-			rVal := reflect.ValueOf(val)
+			rVal := reflect.ValueOf(v)
 			var tArg reflect.Type //Method argument type
 
 			//If true we are in the variadic parameters
@@ -163,7 +161,8 @@ func evalCallExpr(expr *ast.CallExpr, context Context) (interface{}, error) {
 			tVal := rVal.Type() //Passed parameter type
 			if tVal != tArg {
 				if !tVal.ConvertibleTo(tArg) {
-					return nilInterf, fmt.Errorf("Method argument %d type mismatch. Expected %s get %s", key, tArg, tVal)
+					err = fmt.Errorf("Method argument %d type mismatch. Expected %s get %s", key, tArg, tVal)
+					break
 				}
 				rVal = rVal.Convert(tArg)
 			}
@@ -171,12 +170,50 @@ func evalCallExpr(expr *ast.CallExpr, context Context) (interface{}, error) {
 		}
 	}
 	//Call
-	retVal := method.Call(args)
-
-	//Evaluate result
-	if len(retVal) == 0 {
-		return nilInterf, nil
+	if err == nil {
+		retVal = method.Call(args)
+		//Evaluate result
+		if len(retVal) == 0 {
+			val, err = nilInterf, nil
+		} else {
+			val, err = retVal[0].Interface(), nil
+		}
 	}
-	return retVal[0].Interface(), nil
 
+	return
+}
+
+func getMethodToCall(val interface{}, callsite callSite) (method reflect.Value, methodCallable Callable, err error) {
+	var isCallable bool
+	//-------------------Check Method------------------------
+	if !callsite.isValid {
+		//Not a callSite, must be a Ident(args), so Ident must be a function
+		if val == nil {
+			err = fmt.Errorf("Waiting reflect.Func/Callable found %v", "nil")
+		}
+		methodCallable, isCallable = val.(Callable)
+		if !isCallable {
+			method = reflect.ValueOf(val)
+			if method.Kind() != reflect.Func {
+				err = fmt.Errorf("Waiting reflect.Func found %v", reflect.TypeOf(val))
+			}
+		}
+	} else {
+		callableContext, ok := callsite.callee.(CallableContext)
+		if ok {
+			methodCallable, err = callableContext.GetCallable(callsite.fnName)
+			isCallable = (methodCallable != nil && err == nil)
+		} else {
+			isCallable = false
+		}
+		if !isCallable {
+			//A callSite so must be f.x(args)
+			caleeVal := reflect.ValueOf(callsite.callee)
+			method = caleeVal.MethodByName(callsite.fnName)
+			if !method.IsValid() {
+				err = fmt.Errorf("Method %s not found", callsite.fnName)
+			}
+		}
+	}
+	return
 }
